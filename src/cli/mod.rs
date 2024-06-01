2367ec79a7f366dca7b798a57ad3cdf28a251994
luhc228 <luhengchang228@gmail.com>
2024-06-01 11:12:54 +0800

    chore: update manifest json default value

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -19,7 +19,7 @@ pub enum Commands {
 pub struct InstallOpts {
     #[arg(
         long,
-        default_value = "./toolkits.manifest.json",
+        default_value = "https://raw.githubusercontent.com/apptools-lab/AppToolkit/feat/cli/toolkits.manifest.json",
         help = "Path to the toolkits manifest file. You can pass a URL to a remote manifest file or a file path to a local manifest file."
     )]
     pub manifest: String,
