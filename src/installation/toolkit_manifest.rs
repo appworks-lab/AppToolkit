44a84ca0328017319fa1bc9154fcc0711df9193d
luhc228 <luhengchang228@126.com>
2024-06-01 15:29:31 +0800

    fix: test case

--- src/installation/toolkit_manifest.rs
+++ src/installation/toolkit_manifest.rs
@@ -69,7 +69,7 @@ mod test_get_tookits_manifest {
 
     #[tokio::test]
     async fn test_with_remote_file() -> Result<()> {
-        let toolkits_manifest = get_tookits_manifest("https://gist.githubusercontent.com/luhc228/6980b3e72e66066c8d27ef7b3f66580b/raw/a47a3c0b68b5fedf62cd0f7a43c0bc2c224d4d60/toolkit.config.json").await?;
+        let toolkits_manifest = get_tookits_manifest("https://raw.githubusercontent.com/apptools-lab/AppToolkit/feat/cli/toolkits.manifest.json").await?;
         assert!(toolkits_manifest.tools.len() == 3);
         Ok(())
     }
