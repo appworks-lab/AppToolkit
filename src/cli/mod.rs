1743b336ef4027d42c4b104a337fabeffac94115
luhc228 <luhengchang228@126.com>
2024-06-03 20:41:12 +0800

    feat: add install sh

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -11,6 +11,9 @@ pub struct Cli {
 pub enum Commands {
     #[command(name = "install", about = "Install toolkits.")]
     Install(InstallOpts),
+    // TODO: Implement the command
+    // #[command(name = "list", about = "Show the toolkits Details.")]
+    // List(ListOpts),
 }
 
 #[derive(Parser, Debug)]
@@ -21,4 +24,12 @@ pub struct InstallOpts {
         help = "Path to the toolkits manifest file. You can pass a URL to a remote manifest file or a file path to a local manifest file."
     )]
     pub manifest: String,
+}
+
+#[derive(Parser, Debug)]
+pub struct GitSSHOpts {
+    #[arg(long)]
+    user_name: String,
+    #[arg(long)]
+    user_email: String,
 }
